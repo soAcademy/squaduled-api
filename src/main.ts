00d7c8bc03d0d01763984ae18ed2250334c3d9a6
kunlanis <yoo.kunlanis@gmail.com>
2023-03-27 22:18:04 +0700

    set up vercel

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,23 @@ import express, { Application, Request, Response } from "express";
 import axios from "axios";
 import * as t from "io-ts";
 import { AppRoutes } from "./routes";
+import dotenv from "dotenv";
 
 // allow cors origins
-import cors from 'cors';
+import cors from "cors";
 const app: Application = express();
-
+const PORT = process.env.PORT || 3200;
 const corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+dotenv.config();
 app.use(cors(corsOptions));
 
 // Structure : ต้องเพิ่มบรรทัดนี้เพื่อให้ POST method รับข้อมูลแบบ json ได้
 app.use(express.json());
+
+app.get("/", (req, res) => res.send("good health"));
 
 AppRoutes.map((route) => {
   app[route.method as keyof Application](
@@ -26,6 +29,6 @@ AppRoutes.map((route) => {
 });
 
 // Structure :
-app.listen(3200, () => {
-  console.log("Server start on port 3200!");
+app.listen(PORT, () => {
+  console.log(`Server start on port! ${PORT} `);
 });
