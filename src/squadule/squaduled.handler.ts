9f0f02cda2d9921f1ee7f356bfa581a9aa1ddcf4
kunlanis <yoo.kunlanis@gmail.com>
2023-04-03 16:35:21 +0700

    fix : login handler

--- src/squadule/squaduled.handler.ts
+++ src/squadule/squaduled.handler.ts
@@ -441,7 +441,12 @@ export const logInHandler = async (req: Request, res: Response) => {
       //check is office hour
       const result = await logIn(req.body);
 
-      res.status(200).json(result);
+      if (!result.error) {
+        res.status(200).json(result);
+      }else{
+        res.status(401).json(result)
+      }
+      
     } else {
       res.status(404).json({ error: String("Error invalid codec") });
     }
